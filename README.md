1c640fa11c7702cdd7934aef038b7f1a66830407
Kristin Fritsch <ich@kristin-fritsch.de>
2020-01-05 18:03:56 +0100

    Update footer, fixes #5

--- README.md
+++ README.md
@@ -12,3 +12,13 @@ HOW TO:
 1. `npm i`
 2. `npm run dev`
 3. open `http:localhost:3000`
+
+## Styles
+
+The global styles can be found in the `style-global.js` file.
+
+## Grid
+
+We use flexbox grid to position elements on the site.
+
+For general information on how to use the grid please see http://flexboxgrid.com/ and for specific details about the react component see [react-styled-flexboxgrid](https://github.com/LoicMahieu/react-styled-flexboxgrid). As an example implementation you can checkout the [demo](https://loicmahieu.github.io/react-styled-flexboxgrid/demo/index.html).
--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,81 +1,115 @@
-import { Fragment } from 'react'
-// import Link from 'next/link'
-import useMediaQuery from '@material-ui/core/useMediaQuery'
-import FacebookIcon from '@material-ui/icons/Facebook'
-
-import { mediaquery } from '../../style/style'
+import Link from 'next/link'
+import { Grid, Col, Row } from 'react-styled-flexboxgrid'
 
 export default function Footer() {
-  const isDesktop = useMediaQuery('(min-width: 768px)')
-
   return (
     <footer>
-      {!isDesktop && (
-        <Fragment>
-          <h3>Connect</h3>
-          <a>
-            <FacebookIcon />
-          </a>
-          <a>
-            <FacebookIcon />
-          </a>
-          <a>
-            <FacebookIcon />
-          </a>
-          <a>
-            <FacebookIcon />
-          </a>
-        </Fragment>
-      )}
-
-      {isDesktop && (
-        <Fragment>
-          <div>
-            <h3>Get involved</h3>
-            <ul>
-              <li>Organize an event</li>
-              <li>Host an event</li>
-              <li>Become a sponsor</li>
-            </ul>
-          </div>
-          <div>
-            <h3>our values</h3>
-            <ul>
-              <li>Organize an event</li>
-              <li>Host an event</li>
-              <li>Become a sponsor</li>
-            </ul>
-          </div>
-          <div>
-            <h3>About us</h3>
-            <ul>
-              <li>Organize an event</li>
-              <li>Host an event</li>
-              <li>Become a sponsor</li>
-            </ul>
-          </div>
-        </Fragment>
-      )}
+      <div className='grid-wrapper'>
+        <Grid fluid>
+          <Row around='sm'>
+            <Col xs={12} sm={3}>
+              <h3>Get involved</h3>
+              <ul>
+                <li>
+                  <Link href=''>
+                    <a>Join a meetup</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href=''>
+                    <a>Organize an event</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href=''>
+                    <a>Start a chapter</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href=''>
+                    <a>Become a sponsor</a>
+                  </Link>
+                </li>
+              </ul>
+            </Col>
+            <Col xs={12} sm={3}>
+              <h3>Our values</h3>
+              <ul>
+                <li>
+                  <Link href=''>
+                    <a>What drives us</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href=''>
+                    <a>Chore values</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href=''>
+                    <a>Coaching guidelines</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href=''>
+                    <a>Code of Conduct</a>
+                  </Link>
+                </li>
+              </ul>
+            </Col>
+            <Col xs={12} sm={3}>
+              <h3>About us</h3>
+              <ul>
+                <li>
+                  <Link href=''>
+                    <a>Learn more</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href=''>
+                    <a>Press</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href=''>
+                    <a>Imprint</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href=''>
+                    <a>Privacy Policy</a>
+                  </Link>
+                </li>
+              </ul>
+            </Col>
+          </Row>
+        </Grid>
+      </div>
       <style jsx>{`
         footer {
           width: 100%;
+          padding: 72px 0 92px 0;
           background-color: var(--secondaryBlue);
           min-height: 100px;
-          color: white;
-          padding: 40px 25px;
+          font-weight: 500;
+          font-size: 20px;
+          color: #9dd3f2;
+        }
+
+        .grid-wrapper {
+          max-width: 900px;
+          margin: 0 auto;
         }
 
         h3 {
+          font-weight: 800;
+          font-size: 24px;
           color: white;
-          padding-top: 0;
-          text-align: center;
         }
 
-        @media (${mediaquery.tabletToDesktop}) {
-          footer {
-            display: flex;
-            justify-content: space-evenly;
-          }
+        a {
+          color: #9dd3f2;
+          font-weight: 500;
         }
       `}</style>
     </footer>
