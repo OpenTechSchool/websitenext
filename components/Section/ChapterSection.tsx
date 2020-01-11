2218df8acca7174cf75241b46f5e3ce5d4207a94
Kristin Fritsch <ich@kristin-fritsch.de>
2020-01-11 12:04:32 +0100

    Add community page, fixes #2 (#12)

---
+++ components/Section/ChapterSection.tsx
@@ -0,0 +1,68 @@
+import { Grid, Col, Row } from 'react-styled-flexboxgrid'
+import { Link } from '@material-ui/core'
+
+export default function ChapterSection() {
+  return (
+    <section>
+      <div className='content-wrapper'>
+        <Grid fluid>
+          <Row center='xs'>
+            <Col>
+              <span>Active chapter</span>
+            </Col>
+          </Row>
+          <Row between='xs'>
+            <Col>
+              <Link href='/'>
+                <a>Berlin</a>
+              </Link>
+            </Col>
+            <Col>
+              <Link href='/'>
+                <a>Bristol</a>
+              </Link>
+            </Col>
+            <Col>
+              <Link href='/'>
+                <a>Bruessels</a>
+              </Link>
+            </Col>
+            <Col>
+              <Link href='/'>
+                <a>Leipzig</a>
+              </Link>
+            </Col>
+            <Col>
+              <Link href='/'>
+                <a>Zürich</a>
+              </Link>
+            </Col>
+          </Row>
+        </Grid>
+      </div>
+      <style jsx>{`
+        section {
+          font-family: var(--primaryFont);
+          font-weight: 600;
+          font-size: 24px;
+          text-transform: uppercase;
+        }
+
+        a {
+          color: var(--pink);
+        }
+
+        a:hover {
+          color: var(--mainBlue);
+        }
+
+        span {
+          font-family: var(--secondaryFont);
+          font-weight: 500;
+          font-size: 22px;
+          color: #828282;
+        }
+      `}</style>
+    </section>
+  )
+}
