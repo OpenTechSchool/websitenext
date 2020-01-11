5fde8f1911ab6fab2da12a2a76ce0c46bbf0cf6c
Kristin Fritsch <ich@kristin-fritsch.de>
2020-01-11 19:15:17 +0100

    Fix cities section

--- components/Section/ChapterSection.tsx
+++ components/Section/ChapterSection.tsx
@@ -1,10 +1,29 @@
 import { Grid, Col, Row } from 'react-styled-flexboxgrid'
 import Link from 'next/link'
+import matter from 'gray-matter'
 import useTranslation from '../../hooks/useTranslation'
 import TextSection from '../../components/Section/TextSection'
 
 export default function ChapterSection({ title }) {
-  const { t } = useTranslation()
+  const { locale, t } = useTranslation()
+  const cities = (ctx => {
+    // get all keys from data/cities
+    const keys = ctx.keys()
+    // grab the values from these files
+    const values = keys.map(ctx)
+
+    const data = keys.map((key, index) => {
+      const value: any = values[index]
+      const document = matter(value.default)
+
+      return {
+        document,
+      }
+    })
+
+    return data
+    // TODO: Make language a dynamic value
+  })(require.context(`../../data/cities/en`, true, /\.md$/))
 
   return (
     <TextSection classname='' title={title}>
@@ -15,50 +34,26 @@ export default function ChapterSection({ title }) {
               <span>{t('chapter.active')}</span>
             </Col>
           </Row>
-          <Row between='xs'>
-            <Col>
-              <Link href='/'>
-                <a>Berlin</a>
-              </Link>
-            </Col>
-            <Col>
-              <Link href='/'>
-                <a>Bristol</a>
-              </Link>
-            </Col>
-            <Col>
-              <Link href='/'>
-                <a>Bruessels</a>
-              </Link>
-            </Col>
-            <Col>
-              <Link href='/'>
-                <a>Leipzig</a>
-              </Link>
-            </Col>
-            <Col>
-              <Link href='/'>
-                <a>Zürich</a>
-              </Link>
-            </Col>
+          <Row around='xs'>
+            {cities &&
+              cities.map(({ document: { data } }) => (
+                <Col key={data.slug}>
+                  <Link href={{ pathname: `${locale}/city/${data.slug}` }}>
+                    <a>{data.title}</a>
+                  </Link>
+                </Col>
+              ))}
           </Row>
         </Grid>
       </div>
 
       <style jsx>{`
-        h1 {
-          text-align: center;
-        }
-
-        section {
-          font-family: var(--primaryFont);
-          font-weight: 600;
-          font-size: 24px;
-        }
-
         a {
           color: var(--pink);
           text-transform: uppercase;
+          font-family: var(--primaryFont);
+          font-weight: 600;
+          font-size: 24px;
         }
 
         a:hover {
