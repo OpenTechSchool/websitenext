e0927799f090c0117b948231ed43d73afbe9fb77
Kristin Fritsch <ich@kristin-fritsch.de>
2020-01-19 18:38:33 +0100

    Add contact section

---
+++ components/Section/ContactSection.tsx
@@ -0,0 +1,51 @@
+import Grid, { GridDirection } from '@material-ui/core/Grid'
+import { useTheme } from '@material-ui/core/styles'
+import useMediaQuery from '@material-ui/core/useMediaQuery'
+import Button from '../../components/Button/Button'
+import useTranslation from '../../hooks/useTranslation'
+import TextSection from './TextSection'
+
+export default function ContactSection() {
+  const { locale, t } = useTranslation()
+  const theme = useTheme()
+  const direction: GridDirection = useMediaQuery(theme.breakpoints.up('md'))
+    ? 'row'
+    : 'column-reverse'
+
+  return (
+    <TextSection classname='grey'>
+      <Grid container direction={direction} justify='space-between'>
+        <Grid item xs={12} md={5}>
+          <div className='placeholder'></div>
+        </Grid>
+        <Grid item xs={12} md={5}>
+          <h1>{t('homepage.getInTouch.title')}</h1>
+          <p>{t('homepage.getInTouch.description')}</p>
+          <p>{t('homepage.getInTouch.description2')}</p>
+          <br />
+          <p>{t('homepage.getInTouch.description3')}</p>
+          <br />
+          <br />
+          <Grid container justify='space-between'>
+            <Grid item xs={6}>
+              <Button href='https://join.slack.com/t/opentechschool/shared_invite/enQtODg4MDM5MzA0OTMzLWM2ZGE2OWRkNmNmNDkyZGZjZGExMzQxYzMxMDRmOGFmMzUxMWM1YzczOWQ0M2I2MTBkODA4ZWYxYjQyNjk5NGY'>
+                {t('homepage.getInTouch.slack')}
+              </Button>
+            </Grid>
+            <Grid item xs={6}>
+              <Button href='mailto:'>{t('homepage.getInTouch.email')}</Button>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+      <style jsx>{`
+        .placeholder {
+          background: var(--pink);
+          min-height: 500px;
+          width: 100%;
+          margin: 40px 0;
+        }
+      `}</style>
+    </TextSection>
+  )
+}
--- pages/[lang]/index.tsx
+++ pages/[lang]/index.tsx
@@ -1,8 +1,5 @@
 import { NextPage } from 'next'
-import PropTypes from 'prop-types'
-import { useTheme } from '@material-ui/core/styles'
-import useMediaQuery from '@material-ui/core/useMediaQuery'
-import Grid, { GridDirection } from '@material-ui/core/Grid'
+import Grid from '@material-ui/core/Grid'
 import matter from 'gray-matter'
 import Link from 'next/link'
 import useTranslation from '../../hooks/useTranslation'
@@ -12,6 +9,7 @@ import LocalSwitcher from '../../components/LocalSwitcher/LocalSwitcher'
 import TextSection from '../../components/Section/TextSection'
 import ChapterSection from '../../components/Section/ChapterSection'
 import Button from '../../components/Button/Button'
+import ContactSection from '../../components/Section/ContactSection'
 
 interface IndexProps {
   cities?: Array<Record<string, any>>
@@ -19,10 +17,6 @@ interface IndexProps {
 
 export const Index: NextPage<IndexProps> = ({ cities }) => {
   const { t } = useTranslation()
-  const theme = useTheme()
-  const direction: GridDirection = useMediaQuery(theme.breakpoints.up('md'))
-    ? 'row'
-    : 'column-reverse'
 
   return (
     <HomepageLayout
@@ -106,32 +100,7 @@ export const Index: NextPage<IndexProps> = ({ cities }) => {
 
       <ChapterSection title={t('chapter.title')} cities={cities} />
 
-      <TextSection classname='grey'>
-        <Grid container direction={direction} justify='space-between'>
-          <Grid item xs={12} md={5}>
-            <div className='placeholder'></div>
-          </Grid>
-          <Grid item xs={12} md={5}>
-            <h1>{t('homepage.getInTouch.title')}</h1>
-            <p>{t('homepage.getInTouch.description')}</p>
-            <p>{t('homepage.getInTouch.description2')}</p>
-            <br />
-            <p>{t('homepage.getInTouch.description3')}</p>
-            <br />
-            <br />
-            <Grid container justify='space-between'>
-              <Grid item xs={6}>
-                <Button href='https://join.slack.com/t/opentechschool/shared_invite/enQtODg4MDM5MzA0OTMzLWM2ZGE2OWRkNmNmNDkyZGZjZGExMzQxYzMxMDRmOGFmMzUxMWM1YzczOWQ0M2I2MTBkODA4ZWYxYjQyNjk5NGY'>
-                  {t('homepage.getInTouch.slack')}
-                </Button>
-              </Grid>
-              <Grid item xs={6}>
-                <Button href='mailto:'>{t('homepage.getInTouch.email')}</Button>
-              </Grid>
-            </Grid>
-          </Grid>
-        </Grid>
-      </TextSection>
+      <ContactSection />
 
       <style jsx>{`
         .ways-to-join {
