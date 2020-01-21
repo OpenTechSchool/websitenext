a80bc0037d0ed208e3c0e79febbb02c5792b5c6b
Kristin Fritsch <ich@kristin-fritsch.de>
2020-01-21 22:05:32 +0100

    Copy updates for contribute page (#32)
    * Copy updates for contribute page
    
    * Add contact section
    
    * More updates

--- pages/[lang]/contribute.tsx
+++ pages/[lang]/contribute.tsx
@@ -2,11 +2,14 @@ import { NextPage } from 'next'
 import { useTheme } from '@material-ui/core/styles'
 import useMediaQuery from '@material-ui/core/useMediaQuery'
 import Grid, { GridDirection } from '@material-ui/core/Grid'
+import Link from 'next/link'
 import PageLayout from '../../components/PageLayout/PageLayout'
 import LocalSwitcher from '../../components/LocalSwitcher/LocalSwitcher'
 import useTranslation from '../../hooks/useTranslation'
 import WithLocale from '../../containers/withLocale'
 import TextSection from '../../components/Section/TextSection'
+import Button from '../../components/Button/Button'
+import ContactSection from '../../components/Section/ContactSection'
 
 const Community: NextPage = () => {
   const { t } = useTranslation()
@@ -14,8 +17,6 @@ const Community: NextPage = () => {
   const direction: GridDirection = useMediaQuery(theme.breakpoints.up('md'))
     ? 'row'
     : 'column-reverse'
-
-  console.log(direction)
 
   return (
     <PageLayout
@@ -26,45 +27,62 @@ const Community: NextPage = () => {
         <LocalSwitcher />
       </section>
 
-      <TextSection title={t('contribute.howToContribute.title')}>
-        <Grid container justify='space-between'>
+      <TextSection title={t('contribute.contribute.title')}>
+        <Grid container justify='space-around'>
           <Grid item xs={12} md={5}>
-            <h2>{t('contribute.howToContribute.joinChapter')}</h2>
-            <p>
-              Aliquam erat volutpat. Nunc orci enim, scelerisque non rhoncus
-              maximus, vestibulum sit amet elit. Proin venenatis, justo non
-              mattis tristique, lorem justo porta quam, ac pretium sapien eros
-              sed ligula.
-            </p>
-          </Grid>
-          <Grid item xs={12} md={5}>
-            <div className='placeholder'></div>
-          </Grid>
-        </Grid>
-        <Grid container direction={direction} justify='space-between'>
-          <Grid item xs={12} md={5}>
-            <div className='placeholder'></div>
-          </Grid>
-          <Grid item xs={12} md={5}>
-            <h2>{t('contribute.howToContribute.openChapter')}</h2>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
-              efficitur libero ac nunc malesuada mattis. Proin dolor lorem,
-              luctus at purus ut, lacinia feugiat quam. Suspendisse molestie
-              volutpat risus, vel tempus leo mollis ac. Maecenas facilisis augue
-              at gravida efficitur. Pellentesque lectus risus, ultricies ac
-              justo semper, tincidunt fringilla urna. Aliquam erat volutpat.
-              Mauris fermentum diam in velit auctor vestibulum. Sed nisi mi,
-              pharetra sit amet faucibus sed, suscipit sit amet sapien. Sed
-              semper libero nec pulvinar vestibulum.
-            </p>
+            <p>{t('contribute.contribute.description')}</p>
+            <br />
+            <p>{t('contribute.contribute.description2')}</p>
           </Grid>
         </Grid>
       </TextSection>
 
-      <TextSection title={t('contribute.sponsor.title')} anchor='sponsor'>
-        Text
+      <TextSection title={t('contribute.volunteering.title')} classname='grey'>
+        <Grid container justify='space-between'>
+          <Grid item xs={12} md={3}>
+            <h2>{t('contribute.volunteering.coach.title')}</h2>
+            <p>{t('contribute.volunteering.coach.description')}</p>
+            <br />
+            <Link href=''>
+              <a>{t('contribute.volunteering.coach.description2')}</a>
+            </Link>
+          </Grid>
+          <Grid item xs={12} md={3}>
+            <h2>{t('contribute.volunteering.organize.title')}</h2>
+            <p>{t('contribute.volunteering.organize.description')}</p>
+            <br />
+            <Link href=''>
+              <a>{t('contribute.volunteering.organize.description2')}</a>
+            </Link>
+          </Grid>
+          <Grid item xs={12} md={3}>
+            <h2>{t('contribute.volunteering.openChapter.title')}</h2>
+            <p>{t('contribute.volunteering.openChapter.description')}</p>
+            <br />
+            <Link href=''>
+              <a>{t('contribute.volunteering.openChapter.description2')}</a>
+            </Link>
+          </Grid>
+        </Grid>
+        <br />
+        <br />
+        <p>{t('contribute.volunteering.getInTouch')}</p>
       </TextSection>
+
+      <TextSection title={t('contribute.sponsor.title')} anchor='sponsor'>
+        <Grid container justify='space-around'>
+          <Grid item xs={12} md={6}>
+            <p>{t('contribute.sponsor.description')}</p>
+            <br />
+            <p>{t('contribute.sponsor.description2')}</p>
+            <br />
+            <Button href=''>{t('contribute.sponsor.membership')}</Button>{' '}
+            <Button href=''>{t('contribute.sponsor.donation')}</Button>
+          </Grid>
+        </Grid>
+      </TextSection>
+
+      <ContactSection />
 
       <style jsx>{`
         h1 {
@@ -73,7 +91,7 @@ const Community: NextPage = () => {
 
         .placeholder {
           background: var(--pink);
-          min-height: 500px;
+          min-height: 300px;
           width: 100%;
           margin: 40px 0;
         }
