65751aee759a0f93aa01c8e7c2d5964ebdf95b04
xMartin <xmartin@siarp.de>
2020-04-18 13:51:34 +0200

    Fix page titles for open graph and Twitter

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -4,12 +4,13 @@ import useTranslation from '../hooks/useTranslation'
 
 export default function Meta({ pageTitle, pageDescription, pageImage }) {
   const { t } = useTranslation()
+  pageTitle = `${pageTitle} | OpenTechSchool`
   return (
     <>
       <Head>
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <meta charSet='utf-8' />
-        <title>{`${pageTitle} | OpenTechSchool`}</title>
+        <title>{pageTitle}</title>
         {!!pageDescription && (
           <meta name='Description' content={pageDescription} />
         )}
